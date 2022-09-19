1b61d0de080fb0111795903a413193117327c8c5
ajdeepa <108872338+ajdeepa@users.noreply.github.com>
2022-09-19 17:14:56 +0530

    Add files via upload
    Json iterate using For and For in loop
    Json Resume

---
+++ script.js
@@ -0,0 +1,32 @@
+var myResume={
+    "Introduction":[
+        {
+    "Name":"DEEPA A",
+    "Email":"ajdeepacuty@gmail.com",
+    "Mother Tongue":"Tamil",
+    "Date of Birth":"28-11-1993",
+    "Nationality":"Indian",
+    "Marital Status":"Single",
+    "Languages Known":"English and Tamil",
+    "Hobbies":"Listening songs",
+    }
+],
+    "Education":[
+        {
+            "Degree":"BE-CSE",
+            "Institution":"GKM College of Engineering and Technology",
+            "Batch start year":"2011",
+            "Batch end year":"2015",
+        }
+    ],
+    "Work":[
+        {
+            "Company":"GVK Emergency Institute",
+            "Department":"Fleet",
+            "Designation":"Senior Assistant",
+            "Type":"Billing Process",
+            "Experience":"3 years",
+        }
+    ],
+}
+console.log(myResume);
